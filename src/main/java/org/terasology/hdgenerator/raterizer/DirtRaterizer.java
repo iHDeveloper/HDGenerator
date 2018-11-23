9f6f7f5e78c1c2122c829b0f603f1553f230b5c2
iHDeveloper <hamzadevelop@gmail.com>
2018-11-23 09:13:26 +0300

    Add Generate chunks process

--- src/main/java/org/terasology/hdgenerator/raterizer/DirtRaterizer.java
+++ src/main/java/org/terasology/hdgenerator/raterizer/DirtRaterizer.java
@@ -15,5 +15,33 @@
  */
 package org.terasology.hdgenerator.raterizer;
 
-public class DirtRaterizer {
+import org.terasology.math.ChunkMath;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizer;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+
+public class DirtRaterizer implements WorldRasterizer {
+
+    private Block dirt;
+
+    @Override
+    public void initialize() {
+        dirt = CoreRegistry.get(BlockManager.class).getBlock("Core:Dirt");
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        SurfaceHeightFacet facet = chunkRegion.getFacet(SurfaceHeightFacet.class);
+        for (Vector3i position : chunkRegion.getRegion()) {
+            float surfaceHeight = facet.getWorld(position.x, position.z);
+            if (position.y < surfaceHeight) {
+                chunk.setBlock(ChunkMath.calcBlockPos(position), dirt);
+            }
+        }
+    }
 }
